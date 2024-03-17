cb54fdf9939dcc862f66970f073b5bd56b0f3e8f
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2024-03-17 20:55:27 +0800

    Use Iosevka docker image

--- make_font.sh
+++ make_font.sh
@@ -1,8 +1,6 @@
 set -e
 
-docker build -t iosevka-build ./iosevka-docker -f ./iosevka-docker/Dockerfile
-docker run --rm -it -v $(pwd):/build iosevka-build ttf::iosevka-term-mono
-docker run --rm -it -v $(pwd):/build iosevka-build ttf::iosevka-term 
-docker run --rm -it -v $(pwd):/build iosevka-build ttf::iosevka-code 
+docker build -t=fontcc Iosevka/docker
+docker run --rm -it -v $(pwd):/work fontcc ttf::iosevka-term-mono ttf::iosevka-term ttf::iosevka-code 
 docker build -t fontforge-python ./fontforge-python -f ./fontforge-python/Dockerfile
 docker run --rm -it -v $(pwd):/build fontforge-python bash patch_font.sh
